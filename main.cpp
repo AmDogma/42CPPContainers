dd35ccd00e8b1eda0a02301827d7088d15f755e3
mdewayne <sawden@mail.ru>
2021-10-08 00:10:49 +0300

    stack in progress

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "vector.hpp"
+#include "stack.hpp"
+#include <vector>
+#include <stack>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
 
-    Vector<int> myVector;
+    std::vector<int> realVector(22);
+    ft::Vector<int> myVector;
     std::cout << myVector.size() << std::endl;
+
+    std::stack<int, std::vector<int> > stackVector;
+    ft::stack<int> myStack;
+
+    myStack.push(22);
+    stackVector.push(33);
+
+    std::cout << stackVector.top() << " stackVector" << std::endl;
+    std::cout << myStack.top() << " myStack" << std::endl;
 
     return 0;
 }
---
+++ stack.hpp
@@ -0,0 +1,36 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <iostream>
+#include <vector> // need to delete after change to ft::vector
+#include "vector.hpp"
+
+namespace ft{
+    template <class T, class Container = std::vector<T> >
+    class stack {
+        typedef T                value_type;
+        typedef Container    container_type; //
+        typedef std::size_t       size_type;
+    public:
+        explicit stack (const container_type& ctnr = container_type()) : container() {}
+        size_type size() const{
+            return (container.size());
+        }
+        value_type& top(){
+            return (container.back());
+        }
+        const value_type& top() const{
+            return (container.back());
+        }
+        void push (const value_type& val){
+            container.push_back(val);
+        }
+        void pop(){
+            container.pop_back();
+        }
+    private:
+        container_type container;
+    };
+}
+
+#endif
